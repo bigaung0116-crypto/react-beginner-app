7d8a33aca4ce93584329f42e804091df4e381cfb
bigaung0116-crypto <bigaung0116@gmail.com>
2026-05-05 01:40:34 +0800

    Day 1: Innital project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"; 
+function App() {
+  return (
+    <div>
+      <Header />
+      <main style={{padding:'20px' }}>
+        <h3>Welcome to day 1 </h3>
+        <p>Project ကိုအောင်မြင်စွာစတင်နိုင်ပါပြီ</p>
+      </main>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <nav style={{padding: '1rem',
+            background: '#333', color:
+            'white'
+        }}>
+            <h2>My Learning Project</h2>
+        </nav>
+    );
+}
+export default Header;
